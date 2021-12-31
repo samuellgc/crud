d6aabe4e8afa47c879155f33c94a38ed6c9c637d
samuellgc <samuellgc@hotmail.com>
2021-12-31 11:15:25 -0300

    JS modification and project concluded

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,54 +1,9 @@
-const openModal = () => {
-    modalContainer.classList.remove('invisible');
-}
-const openModalEdit = () => {
-    modalContainer.classList.remove('invisible');
-    btnSaveChanges.classList.remove('invisible');
-    btnSave.remove()
-    document.getElementById('btns-modal').insertBefore(btnSaveChanges, btnCancel);
-}
 const closeModal = () => modalContainer.classList.add('invisible');
 const cleanForm = () => document.getElementById('form-modal').reset();
 const btnCancel = document.getElementById('btn-cancel');
 const btnSave = document.getElementById('btn-save');
 const modalContainer = document.getElementById('modal-container');
 const btnSaveChanges = document.getElementById('btn-save-changes');
-
-document.getElementById('btn-add').addEventListener('click', (e) => {
-    e.preventDefault();
-    openModal();
-})
-
-btnSaveChanges.addEventListener('click', (e) => {
-   e.preventDefault();
-   attCour();
-   closeModal();
-   cleanForm();
-   
-})
-
-
-btnCancel.addEventListener('click', (e) => {
-    e.preventDefault();
-    closeModal();
-    cleanForm();
-})
-
-btnSave.addEventListener('click', (e) => {
-    e.preventDefault();
-    createCourse();
-    closeModal();
-    cleanForm();
-})
-
-//function deleteCard(event) {
-//    event.target.parentNode.parentNode.remove();
-//}
-
-function editModal() {
-    openModalEdit();
-    showCourse();
-}
 
 var dataCourses = [
     {
@@ -58,39 +13,34 @@ var dataCourses = [
         'description': 'newDescriptionCourse'
     }
 ];
-const attCour = () => {
-    let newIdCourse = document.getElementById('add-course-id').value
-    let newNameCourse = document.getElementById('add-course-name').value
-    let newImageCourse = document.getElementById('add-course-img').value
-    let newDescriptionCourse = document.getElementById('add-course-descp').value
 
-    if(newIdCourse == ""){
-        window.alert('Digite um ID válido!')
-        return false;
-    }    
+document.getElementById('btn-add').addEventListener('click', (e) => {
+    e.preventDefault();
+    modalContainer.classList.remove('invisible');
+    btnSaveChanges.classList.add('invisible');
+    btnSave.classList.remove('invisible');
+    document.getElementById('btns-modal').insertBefore(btnCancel, btnSaveChanges);
+    document.getElementById('btns-modal').insertBefore(btnSave, btnCancel);
+    cleanForm();
+})
 
-    for(let i = 0; i < dataCourses.length; i++) {        
-        if (dataCourses[i]['id'] == newIdCourse){                       
-            return window.alert('Esse ID de curso já existe!');
-        }        
-    }
+btnCancel.addEventListener('click', (e) => {
+    e.preventDefault();
+    closeModal();
+    cleanForm();
+})
 
-    dataCourses.push({
-        'id': newIdCourse,
-        'name': newNameCourse,
-        'image': newImageCourse,
-        'description': newDescriptionCourse
-    })
+btnSaveChanges.addEventListener('click', (e) => {
+    e.preventDefault();
+    attCourse();
+})
 
-    const attCourse = document.getElementById('first-element')
-    attCourse.innerHTML = `
-    <th scope="row" id="id-course">${newIdCourse}</th>
-    <td class="name-course" id="name-course">${newNameCourse}</td>
-    <td> <img id="img-course-new" class="img-course img-fluid table-img" src="${newImageCourse}" alt=""></td>
-    <td class="descrip-course" id="descrip-course">${newDescriptionCourse}</td>
-    <td class="btn-action"><button class="btn btn-secondary btn-edit" id="btn-edit" onclick="editModal()">EDIT</button> <button class="btn btn-danger btn-delete" onclick="deleteCard(event)">DELETE</button></td>
-    `
-}
+btnSave.addEventListener('click', (e) => {
+    e.preventDefault();
+    createCourse();
+    closeModal();
+    cleanForm();
+})
 
 const createCourse = () => {
     let newIdCourse = document.getElementById('add-course-id').value
@@ -122,29 +72,42 @@ const createCourse = () => {
     <td class="name-course" id="name-course">${newNameCourse}</td>
     <td> <img id="img-course-new" class="img-course img-fluid table-img" src="${newImageCourse}" alt=""></td>
     <td class="descrip-course" id="descrip-course">${newDescriptionCourse}</td>
-    <td class="btn-action"><button class="btn btn-secondary btn-edit" id="btn-edit" onclick="editModal()">EDIT</button> <button class="btn btn-danger btn-delete" onclick="deleteCard(event)">DELETE</button></td>
+    <td class="btn-action"><button class="btn btn-secondary btn-edit" id="btn-edit" onclick="editModal(${newIdCourse})">EDIT</button> <button class="btn btn-danger btn-delete" onclick="deleteCard(${newIdCourse})">DELETE</button></td>
     `
-    newCourse.id = 'first-element'
-    var firstChild = document.getElementById('first-element')
-    document.getElementById('container-body').insertBefore(newCourse, firstChild);
-}  
+    newCourse.setAttribute('id', `${newIdCourse}`);
+    document.getElementById('container-body').appendChild(newCourse);
+} 
 
-function showCourse() {
-    var courseId = document.getElementById('id-course') 
-    var courseName = document.getElementById('name-course')
-    var courseImg = document.getElementById('img-course-new')
-    var courseDesc = document.getElementById('descrip-course')
-
-    document.getElementById('add-course-id').value = `${courseId.innerHTML}`
-    document.getElementById('add-course-name').value = `${courseName.innerHTML}`
-    document.getElementById('add-course-img').value = `${courseImg.src}`
-    document.getElementById('add-course-descp').value = `${courseDesc.innerHTML}`
+function editModal(id) {    
+    modalContainer.classList.remove('invisible');
+    btnSaveChanges.classList.remove('invisible');
+    document.getElementById('btns-modal').insertBefore(btnSaveChanges, btnSave);
+    btnSave.classList.add('invisible')
+    document.getElementById('btns-modal').insertBefore(btnCancel, btnSave);
+    
+    for(let i = 0; i < dataCourses.length; i++) {        
+        if (dataCourses[i]['id'] == id){
+            document.getElementById('add-course-id').value = dataCourses[i]['id'];
+            document.getElementById('add-course-name').value = dataCourses[i]['name'];
+            document.getElementById('add-course-img').value = dataCourses[i]['image'];
+            document.getElementById('add-course-descp').value = dataCourses[i]['description'];
+        }        
+    } 
 }
 
-function deleteCard(id) {    
-    document.getElementById('first-element').remove();
-    for(let i = 1; i < dataCourses.length; i++) {        
-        if (dataCourses[i]){
+const attCourse = () => {
+    let atualizaCurso = document.getElementById('add-course-id').value
+
+    editModal();
+    deleteCard(atualizaCurso);
+    createCourse();
+    closeModal();
+}
+
+const deleteCard = (id) => {    
+    document.getElementById(id).remove();
+    for(let i = 0; i < dataCourses.length; i++) {        
+        if (dataCourses[i]['id'] == id){
             dataCourses.splice(i, 1);
         }        
     }
